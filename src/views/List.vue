94b161b06bbdbe2e77e7722389dbe7dcd5486b33
Thuanny <thucatm@gmail.com>
2022-01-12 19:32:57 -0300

    listar tarefas

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,9 +1,27 @@
 <template>
   <div class="container mt-2">
-    <b-card title="Titulo">
-      <b-card-text>descrição</b-card-text>
-      <b-button variant="outline-secondary" class="mr-2">Editar</b-button>
-      <b-button variant="outline-danger" class="mr-2">Excluir</b-button>
-    </b-card>
+    <div v-for="(task, index) in tasks" :key="index">
+      <b-card :title="task.subject" class="mb-2">
+        <b-card-text>{{ task.description }}</b-card-text>
+        <b-button variant="outline-secondary" class="mr-2">Editar</b-button>
+        <b-button variant="outline-danger" class="mr-2">Excluir</b-button>
+      </b-card>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "List",
+
+  data() {
+    return {
+      tasks: []
+    }
+  },
+
+  created() {
+    this.tasks = (localStorage.getItem("tasks")) ? JSON.parse(localStorage.getItem("tasks")) : [];
+  }
+}
+</script>
