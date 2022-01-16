dd4cc3caa1b80973db064dd86624ce4a751ade4c
Thuanny <thucatm@gmail.com>
2022-01-16 12:43:19 -0300

    efeito de transição de rotas

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,20 @@
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
-    <router-view/>
+    <transition name="fade" mode="out-in">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
 <style>
+.fade-enter-active, .fade-leave-active {
+  transition-duration: 0.2s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
 
+.fade-enter, .fade-leave {
+  opacity: 0;
+}
 </style>
